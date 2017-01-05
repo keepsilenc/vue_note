9ec1c8f8ae1220025df4ed0dfb35863328e6b84e
keepsilenc <1040399130@qq.com>
2017-01-05 18:03:51 +0800

    搜索 标题 时间排序

---
+++ components/Editor.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="note-editor">
+
+    <input type="text" :value="activeNoteTittle" @input="editTittle" @blur="clearTittle" class="form-control">
+    <textarea
+      :value="activeNoteText"
+      @input="editNote"
+      class="form-control">
+    </textarea>
+  </div>
+</template>
+
+<script>
+import { editNote } from '../vuex/actions'
+import { editTittle } from '../vuex/actions'
+
+export default {
+
+  vuex: {
+    getters: {
+      activeNoteText: state => state.activeNote.text,
+      activeNoteTittle: state => state.activeNote.tittle
+    },
+    actions: {
+      editNote,
+        editTittle
+    }
+  }
+}
+</script>
